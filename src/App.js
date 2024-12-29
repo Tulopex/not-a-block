c4b7a59fa534bde4cfde48af2a027d80f9e1b11e
Tulopex <andoskinvadim2022@gmail.com>
2024-12-29 13:09:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Styles/App.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGamepad, faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 import Game from './Components/Game';
+import Tasks from './Components/Tasks';
 
 function App() {
+  const [isGameVisible, setIsGameVisible] = useState(true);
+
+  const toggleContent = (content) => {
+    if (content === 'game') {
+      setIsGameVisible(true);
+    } else {
+      setIsGameVisible(false);
+    }
+  };
+
   return (
     <div className='App'>
-      <Game />
+      {/* Отображаем либо игру, либо задачи */}
+      {isGameVisible ? <Game /> : <Tasks />}
+
       <div className="footer">
-            <div className="content">
-              <div className="icon">
-                <FontAwesomeIcon icon={faGamepad} />
-                <FontAwesomeIcon icon={faClipboard} />
-              </div>
-            </div>
+        <div className="content">
+          <div className="icon">
+            {/* Иконка игры */}
+            <FontAwesomeIcon
+              icon={faGamepad}
+              onClick={() => toggleContent('game')}
+            />
+            {/* Иконка задач */}
+            <FontAwesomeIcon
+              icon={faClipboard}
+              onClick={() => toggleContent('tasks')}
+            />
           </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Tasks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Tasks.css';
+
+
+function Tasks() {
+  return (
+    <div className='Tasks'>
+      <h1>Tasks</h1>
+    </div>
+  );
+}
+
+export default Tasks;
