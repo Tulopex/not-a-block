e594668916fd6d2b87ec545932b84fb3c4669f36
Tulopex <andoskinvadim2022@gmail.com>
2024-12-29 17:28:10 +0300

    test

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import '../Styles/Tasks.css';
 
-
 function Tasks() {
   return (
     <div className='Tasks'>
       <h1 className='tasks-title'>Tasks</h1>
       <div className='daily-task-container'>
         <ul className='daily-task-list'>
-            <li>
-                <div className='daily-task'>
-                    <h3 className='name-task' id='name-task'>Сheck-in</h3>
-                    <button className='execute-btn' id='execute-btn'>Start</button>
-                </div>
-            </li>
-            <li className='divider'></li>
-            <li>
-              <div className='daily-task'>
-                <h3 className='name-task' id='name-task'>Donate 10 Stars</h3>
-                <button className='execute-btn' id='execute-btn'>Start</button>
+          <li>
+            <div className='daily-task'>
+              <div className='task-details'>
+                <h3 className='name-task'>Check-in</h3>
+                <p className='reward'>+100 Block</p>
               </div>
-            </li>
-            <li className='divider'></li>
-            <li>
-              <div className='daily-task'>
-                <h3 className='name-task' id='name-task'>Promote Blockchain</h3>
-                <button className='execute-btn' id='execute-btn'>Start</button>
+              <button className='execute-btn'>Start</button>
+            </div>
+          </li>
+          <li className='divider'></li>
+          <li>
+            <div className='daily-task'>
+              <div className='task-details'>
+                <h3 className='name-task'>Donate 10 Stars</h3>
+                <p className='reward'>+100 Block</p>
               </div>
-            </li>
+              <button className='execute-btn'>Start</button>
+            </div>
+          </li>
+          <li className='divider'></li>
+          <li>
+            <div className='daily-task'>
+              <div className='task-details'>
+                <h3 className='name-task'>Promote Blockchain</h3>
+                <p className='reward'>+100 Block</p>
+              </div>
+              <button className='execute-btn'>Start</button>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
