2db5b6a4c98c34f0a518ad08c110ceeb6d0a9ac1
Tulopex <andoskinvadim2022@gmail.com>
2024-12-29 15:10:11 +0300

    test

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -6,6 +6,18 @@ function Tasks() {
   return (
     <div className='Tasks'>
       <h1>Tasks</h1>
+      <div className='daily-task-container'>
+        <ul className='daily-task-list'>
+            <li>
+                <div className='daily-task'>
+                    <h3 className='name-task' id='name-task'>Сheck-in</h3>
+                    <button className='execute-btn' id='execute-btn'>Start</button>
+                </div>
+            </li>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
