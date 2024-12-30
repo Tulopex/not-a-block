725f46cd3ee96fe610442194017bbc9b3aee29c0
Tulopex <andoskinvadim2022@gmail.com>
2024-12-30 10:35:29 +0300

    test

--- src/Components/Friends.js
+++ src/Components/Friends.js
@@ -5,7 +5,19 @@ import '../Styles/Friends.css';
 function Friends() {
   return (
     <div className='Friends'>
-      <h1>Friends</h1>
+      <img className='gif-friends' src={process.env.PUBLIC_URL + '/img/friends.gif'} alt='friends' />
+      <h1 className='friends-title'>Friends</h1>
+      <p className='friends-text'>Invite your friends and get 10% of their earned Block</p>
+      <div className='friends-content'>
+        <ul className='friends-list'>
+          <li className='friends-item'>
+            <div className='friends-info'>
+              <h4 className='name-friend'><span id='name-friend'>Tulopex</span></h4>
+            </div>
+            <h4 className='points-friend'><span id='points-friend'>100</span> Block</h4>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -10,24 +10,24 @@ function Leaderboard() {
         <ul className='leaderboard-list'>
           <li className='leaderboard-item'>
             <div className='leaderboard-info'>
-              <h4 className='place'>1.</h4>
-              <h4 className='name'>Morpheus</h4>
+              <h4 className='place'><span id='place'>1</span>.</h4>
+              <h4 className='name'><span id='name'>Morpheus</span></h4>
             </div>
-            <h4 className='points'>10.234.567 Block</h4>
+            <h4 className='points'><span id='points'>10.234.567</span> Block</h4>
           </li>
           <li className='leaderboard-item'>
             <div className='leaderboard-info'>
-              <h4 className='place'>2.</h4>
-              <h4 className='name'>Andrew</h4>
+              <h4 className='place'><span id='place'>2</span>.</h4>
+              <h4 className='name'><span id='name'>Neo</span></h4>
             </div>
-            <h4 className='points'>800 Block</h4>
+            <h4 className='points'><span id='points'>800.000</span> Block</h4>
           </li>
           <li className='leaderboard-item'>
             <div className='leaderboard-info'>
-              <h4 className='place'>3.</h4>
-              <h4 className='name'>Tulopex</h4>
+              <h4 className='place'><span id='place'>3</span>.</h4>
+              <h4 className='name'><span id='name'>Trinity</span></h4>
             </div>
-            <h4 className='points'>500 Block</h4>
+            <h4 className='points'><span id='points'>100.000</span> Block</h4>
           </li>
         </ul>
       </div>
