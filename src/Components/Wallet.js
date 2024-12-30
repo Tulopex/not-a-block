3c8388969745658c683299df6ce05956b9a2d482
Tulopex <andoskinvadim2022@gmail.com>
2024-12-30 11:28:52 +0300

    test

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -5,7 +5,9 @@ import '../Styles/Wallet.css';
 function Wallet() {
   return (
     <div className='Wallet'>
-      
+      <div className='content-points'>
+        <h2 className='points-score'><span id='points-score'>0</span> Block</h2>
+      </div>
     </div>
   );
 }
