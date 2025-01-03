afc26bd29698afb967e4fbe3e0551a5227bf6f06
Tulopex <andoskinvadim2022@gmail.com>
2025-01-03 15:59:07 +0300

    test

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ const initializeTelegramSDK = async () => {
     await miniApp.ready();
 
     await initData(miniApp);
-    
+    const user = await miniApp.getUser();
+    console.log(`ID: ${user.id}, First Name: ${user.first_name}`);
+
     if (miniApp.mount.isAvailable()) {
       miniApp.mount();
       miniApp.isMounted(); // true
